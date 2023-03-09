acaf17a7ccae111134be7de844ef37c76ef4e9dd
viroovr <tjgusdnjs789@gmail.com>
2023-03-09 19:09:11 +0900

    add README, main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from typing import List
-def twoSum(nums: List[int], target: int) -> List[int]:
+class Solution:
+	def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-	for i in range(len(nums)):
-		for j in range(i+1, len(nums)):
-			if nums[i]+nums[j] == target:
-				return [i, j]
-	return []
+		for i in range(len(nums)):
+			for j in range(i+1, len(nums)):
+				if nums[i]+nums[j] == target:
+					return [i, j]
+		return []
 
 nums = [2, 7, 11, 15]
 target = 9
-print(twoSum(nums=nums, target = target))
+
+s = Solution()
+
+print(s.twoSum(nums, target))
 
